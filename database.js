45423699cc502aaa88f7082b73c1a705510a5d45
meowkt23 <30217543@blackpool.ac.uk>
2023-12-20 15:22:46 +0000

    modified:   database.js

--- database.js
+++ database.js
@@ -1,2 +1,4 @@
-const mongoose = require('mongoose');
-mongoose.connect('https://healthway-hospitals-mongo-db.mongo.cosmos.azure.com:443/', { useNewUrlParser: true, useUnifiedTopology: true });
+var mongoClient = require("mongodb").MongoClient;
+mongoClient.connect("mongodb://healthway-hospitals-mongo-db:M2Wa1iLUAet2zSO4cwngAkm3ggLWahsnryPTckHwDayLNB33BmUeiPHT9jQjEloWVJ9IsXDCXGEWACDbnze9ig%3D%3D@healthway-hospitals-mongo-db.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@healthway-hospitals-mongo-db@", function (err, client) {
+  client.close();
+});
