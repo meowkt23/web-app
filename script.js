fd064dc46c8818d7181493d048e424e99b2e1739
meowkt23 <30217543@blackpool.ac.uk>
2023-12-20 17:20:55 +0000

    modified:   script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 function fetchStaff() {
-    //fetch data from '/api/staff' endpoint
+    // Fetch data from '/api/staff' endpoint
     fetch('/api/staff')
-    .then(response => response.json())
-    .then(data => displayStaff(data))
-    .catch(error => console.error('Error fetching staff:', error));
+        .then(response => response.json())
+        .then(data => displayStaff(data))
+        .catch(error => console.error('Error fetching staff:', error));
 }
 
 function displayStaff(staffArray) {
-    //get HTML element with the ID 'staff-data'
+    // Get HTML element with the ID 'staff-data'
     const staffSection = document.getElementById('staff-data');
-    //iterate through each staff object in array
+    // Iterate through each staff object in the array
     staffArray.forEach(individualStaff => {
-        //create new 'div element
+        // Create a new 'div' element
         const div = document.createElement('div');
-        //set inner HTML of 'div' with staff info
-        div.innerHTML = `<p>Staff ID: ${staff.staffId}</p>
-                         <p>First Name: ${staff.firstName}</p>
-                         <p>Last Name: ${staff.lastName}</p>`;
-        //append 'div' to the 'staff-data' section
+        // Set inner HTML of 'div' with staff info
+        div.innerHTML = `<p>Staff ID: ${individualStaff.staffId}</p>
+                         <p>First Name: ${individualStaff.firstName}</p>
+                         <p>Last Name: ${individualStaff.lastName}</p>`;
+        // Append 'div' to the 'staff-data' section
         staffSection.appendChild(div);
-    })
+    });
 }
 
-document.getElementById('staff-form').addEventListener('submit', function(e) {
+document.getElementById('staff-form').addEventListener('submit', function (e) {
     e.preventDefault();
-})
+});
