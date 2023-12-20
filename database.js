c9cfe675484ebeea427284db05ba10a993f0b0dd
meowkt23 <30217543@blackpool.ac.uk>
2023-12-20 17:27:30 +0000

    modified:   database.js

--- database.js
+++ database.js
@@ -1,4 +1,12 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://healthway-hospitals-mongo-db:M2Wa1iLUAet2zSO4cwngAkm3ggLWahsnryPTckHwDayLNB33BmUeiPHT9jQjEloWVJ9IsXDCXGEWACDbnze9ig%3D%3D@healthway-hospitals-mongo-db.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@healthway-hospitals-mongo-db@', function (err, client) {
-  client.close();
+
+const connectionString = process.env.MONGO_CONNECTION_STRING;
+
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function () {
+  console.log('Connected to MongoDB');
 });
