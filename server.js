8e8d87a0445d2af0b78e4fc8db6613acac6551c9
meowkt23 <30217543@blackpool.ac.uk>
2023-12-20 15:44:39 +0000

    modified:   server.js

--- server.js
+++ server.js
@@ -2,5 +2,19 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const staff
+const staff = require('./staff');
+const staff = require('./staff');
 
+app.get('/staff', async (req, res) => {
+    const staff = await staff.find();
+    res.json(appointments);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__direname + 'index.html');
+});
+
+app.use(express.static('public'));
