9f6fb75a64b98636afff9be3b33fba39749e511f
meowkt23 <30217543@blackpool.ac.uk>
2023-12-20 16:36:35 +0000

    modified:   server.js

--- server.js
+++ server.js
@@ -9,17 +9,13 @@ app.get('/staff', async (req, res) => {
     res.json(appointments);
 });
 
-//const PORT = process.env.PORT || 3000;
-//app.listen(PORT, () => console.log('Server running on port ${PORT}'));
-
-//app.get('/', (req, res) => {
-//    res.sendFile($ 'https://github.com/meowkt23/web-app/index.html');
-//});
-
-const path = require('path');  // Make sure to require the 'path' module
-
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile('https://raw.https://github.com/meowkt23/web-app/index.html');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 
 app.use(express.static('public'));
